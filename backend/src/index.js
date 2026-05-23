2a24ed2acfed868d546f327ed0eff330f456d1a0
Adullah Khetran <khetranabdullah@gmail.com>
2026-05-23 07:18:50 +0500

    added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
 import usersRouter from "../routes/users.js";
 import healthRouter from "../routes/health.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"]
+  })
+)
 
 app.use("/health", healthRouter);
 app.use("/users", usersRouter);
